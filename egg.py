a45dea9ef8506e23ffe998c987f35558b843ff42
halley <demeha@ssfs.org>
2021-09-22 15:16:00 -0400

    Started unit 3

--- egg.py
+++ egg.py
@@ -1,6 +1,14 @@
 # Start writing your functions below this line
 
-
+def tophalf():
+    print("  _____  ")
+    print( "/     \\")
+    print("/       \\")
+def bottomhalf():
+    print("\\       /")
+    print(" \______/")
+def line():
+    print(-"-'-"-'-"-)
 def main():
     # all of your function calls should go here. Remove the word "pass" before adding function calls.
     pass
