0caf50e6a73e27ce36df98b03dc889e0191aacdf
halley <demeha@ssfs.org>
2021-09-30 14:29:38 -0400

    third unit assignment finished

--- Tests/assignment_three_tests.py
+++ Tests/assignment_three_tests.py
@@ -1,13 +1,15 @@
 import unittest
-import assignment_three
+from assignment_three import surface_area
+from assignment_three import area_of_rectangle
+
 
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(10, assignment_three.area_of_rectangle(5,2))
-        self.assertEqual(12,assignment_three.area_of_rectangle(4,3))
-        self.assertEqual(150, assignment_three.surface_area(4,3,9))
-        self.assertEqual(72, assignment_three.surface_area(3,2,6))
+        self.assertEqual(10, area_of_rectangle(5,2))
+        self.assertEqual(12, area_of_rectangle(4,3))
+        self.assertEqual(150, surface_area(4,3,9))
+        self.assertEqual(72, surface_area(3,2,6))
 if __name__ == '__main__':
     unittest.main()
--- assignment_three.py
+++ assignment_three.py
@@ -1,12 +1,50 @@
+### Halley Deme
+## Date Last Modified : 09/30/2021
+# This Program asks a user for three parameters and finds the surface area of a rectangular prism, using those parameters.
+
+
 def area_of_rectangle(l,w):
     """
     Calculating the Area of a rectangle
     :param l: Length of rectangle
-    :param w:
-    :return:
+    :param w:width of rectangle.
+    :return:area of rectangle
     """
-    a = l *w
+    a = int(l) * int(w)
     return a
+
+def instructions():
+    """
+    Tells users instructions.
+
+    """
+    print("This program calculates the surface area of a rectangular prism.")
+
+def ask_width():
+    """
+    Asking user for width
+    :return: width of prism
+    """
+    w = input("Enter the width of the prism.")
+    return(w)
+
+def ask_length():
+    """
+    Asking user for width
+    :return: width of prism
+    """
+    l = input("Enter the length of the prism.")
+    return(l)
+
+
+def ask_height():
+    """
+    Asking user for height
+    :return: height of prism
+    """
+    h = input("Enter the height of the prism.")
+    return h
+
 def surface_area(l,w,h):
     """
     Calculating the surface area of a rectangular prism
@@ -15,18 +53,16 @@ def surface_area(l,w,h):
     :param h: height of prism
     :return: the product of length, width, and height
     """
+    # This is the formula for surface area of a rectangular prism.
     sa = area_of_rectangle(l,w) * 2 + area_of_rectangle(w,h) * 2 + area_of_rectangle(l,h) * 2
     return sa
 
-def instructions():
-    print("This program calculates the surface area of a rectangular prism.")
+def main():
+    instructions()
+    w = ask_width()
+    l = ask_length()
+    h = ask_height()
+    print("The surface area is:", surface_area(w,l,h))
 
-def ask_width():
-    print("Enter the width of the prism.")
-
-def ask_length():
-    print("Enter the length of the prism.")
-
-
-def ask_height():
-    print("Enter the height of the prism.")
+if __name__ == '__main__':
+    main()
